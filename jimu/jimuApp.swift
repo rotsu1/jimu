d74384b5119f8faef4fe5110c0fdb0f37f4b5035
rotsu1 <ryuotsu123@gmail.com>
2026-01-14 16:14:56 +1000

    First commit

---
+++ jimu/jimuApp.swift
@@ -0,0 +1,17 @@
+//
+//  jimuApp.swift
+//  jimu
+//
+//  Created by 乙津　龍　 on 14/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct jimuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
